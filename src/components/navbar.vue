9d9c905213c941dfec96b727d490478812a7ba6d
Abdirahmaan12 <abdirahmaanmoha2022@gmail.com>
2025-07-14 17:26:31 +0300

    navbar creation process

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,21 +1,47 @@
 <template>
-  <div class="">
+  <div class="navbar">
     <div class="absolute top-0 left-0 right-0 h-[300px]"
       :class="[
         'transition-colors duration-300',
         isScrollingUp ? 'bg-[#1b4b76]' : 'bg-white',
       ]"
     >
-    <div
+     <div
         :class="[
         'transition-colors duration-300',
         isScrollingUp ? 'bg-[#1b4b76]' : 'bg-[#051662]',
       ]"
-    class="text-center fixed top-5 left-[130px]  right-5 rounded-lg h-[60px]  font-bold  text-white text-[20px]">
-        <p>this is a test</p>
+    class=" flex justify-between   fixed top-5 left-[130px]  right-5 rounded-lg h-[60px]">
+    
+    <div class="container-1 ml-3">
+      <router-link to="/" class="mr-1 text-[#eff1f1] font-light">
+        App  <i class="fa-solid fa-greater-than text-[8px] ml-1 text-[#6387a1]"></i>
+      </router-link>
+      <router-link to="/" class="mr-1 text-[#eff1f1] font-light">
+        Dashboards  <i class="fa-solid fa-greater-than text-[8px] ml-1 text-[#6387a1]"></i>
+      </router-link>
+      <router-link to="/" class="mr-1 text-[#6387a1] font-light">
+        Analytics  
+      </router-link>
+      
+      
+     
+    </div>
+
+     <div class="container-1">
+            <!-- <i class="fa-regular fa-magnifying-glass absolute"></i> -->
+
+        
+    </div>
+
+     <div class="container-1">
+      <h3>hello</h3>
+    </div>
+
     </div>
     </div>
   </div>
+
 </template>
 
 <script>
