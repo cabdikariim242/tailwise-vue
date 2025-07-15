d87cc6ee16a9122e4115cc0007e3e5a2f09ead95
cabdiakariim <cabdikariimmaxamaddaahir@gmail.com>
2025-07-15 10:37:25 +0300

    working on sidebar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -9,7 +9,7 @@
     <div
         :class="[
         'transition-colors duration-300',
-        isScrollingUp ? 'bg-[#1b4b76]' : 'bg-[#051662]',
+        isScrollingUp ? 'bg-[#1b4b76]' : 'bg-[#051662] ml-10',
       ]"
     class="text-center fixed top-5 left-[115px]  right-5 rounded-lg h-[60px]  font-bold  text-white text-[20px]">
         <p>this is a test</p>
--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -2,12 +2,12 @@
   <div class="z-10 relative">
     <div 
       style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.6)"
-      class="flex flex-col bg-[#fff] fixed bottom-3 top-0 left-3 transition-all duration-300 hover:w-[300px] w-[80px] mr-10 mt-5 rounded-lg"
+      class="hidden lg:flex md:flex flex-col  bg-[#fff] group fixed bottom-3 top-0 left-3 transition-all duration-300 hover:w-[300px] w-[80px] mr-10 mt-5 rounded-lg"
     >
     <!-- all icons are here -->
-<div class="flex ">
-    <i class="fa-brands fa-stackpath text-[40px] pl-5 pr-3 pt-3 text-5 rounded "></i>
-    <p class="mt-3 hidden hover:block text-black">ECHO</p>
+<div class="flex  ">
+    <i class="fa-brands fa-stackpath text-[35px] pl-5 pr-3 pt-3 text-5 transition-transform duration-50 rounded-full group-hover:rotate-[360deg]  "></i>
+    <p class="mt-5 group-hover:flex hidden text-[#475569] font-[500] ">ECHO</p>
 </div>
 
 
